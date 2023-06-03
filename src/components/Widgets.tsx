4e64a1568dcf72c1e398ecefe22f0c624b738964
Emmanuel Yeboah <noelzappy@gmail.com>
2023-06-03 03:03:18 +0000

    chore: minor updates

--- src/components/Widgets.tsx
+++ src/components/Widgets.tsx
@@ -43,7 +43,7 @@ export default function Widgets({ newsResults, randomUsersResults }) {
         </button>
       </div>
       <div className="sticky top-16 text-gray-700 space-y-3 bg-gray-100 pt-2 rounded-xl w-[90%] xl:w-[75%]">
-        <h4 className="font-bold text-xl px-4">Who to follow</h4>
+        <h4 className="font-bold text-xl px-4">Topics to follow</h4>
         <AnimatePresence>
           {randomUsersResults?.slice(0, randomUserNum).map((randomUser) => (
             <motion.div
