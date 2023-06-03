edaaf2ec501ac0a4ea02f0fe3b0c6c78043a239d
Emmanuel Yeboah <noelzappy@gmail.com>
2023-06-03 13:37:45 +0000

    feat: added posts by category

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -4,9 +4,8 @@ export default function MenuItem({ text, active, onClick }) {
       className="hoverEffect flex items-center text-gray-700 justify-center xl:justify-start text-lg space-x-3"
       onClick={onClick}
     >
-      <span className={`${active && "font-bold"} hidden xl:inline`}>
-        # {text}
-      </span>
+      <span className={`xl:inline`}>#</span>
+      <span className={`hidden xl:inline`}>{text}</span>
     </div>
   );
 }
