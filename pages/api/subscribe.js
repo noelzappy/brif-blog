3983bc76463a7669949c0c1d575ae2692a06a881
Emmanuel Yeboah <noelzappy@gmail.com>
2024-05-09 19:08:42 +0000

    chore: minor update

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -3,35 +3,37 @@
 import { API_URL } from "@/libs/contentApi";
 
 export default async (req, res) => {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
+  try {
+    if (req.method !== "POST") {
+      return res.status(405).json({ error: "Method not allowed" });
+    }
 
-  const { email, name } = req.body;
+    const { email, name } = req.body;
 
-  if (!email || !name) {
-    return res.status(400).json({ error: "Email and Name is required" });
-  }
+    if (!email || !name) {
+      return res.status(400).json({ error: "Email and Name is required" });
+    }
 
-  const _res = await fetch(API_URL + "/members/api/send-magic-link/", {
-    method: "POST", // or 'PUT'
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      email,
-      name,
-      emailType: "subscribe",
-      labels: [],
-      autoRedirect: true,
-      urlHistory: [],
-      newsletters: [{ name: "BrifHQ" }],
-    }),
-  });
+    await fetch(API_URL + "/members/api/send-magic-link/", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        name,
+        emailType: "subscribe",
+        labels: [],
+        autoRedirect: true,
+        urlHistory: [],
+        newsletters: [{ name: "BrifHQ" }],
+      }),
+    });
 
-  if (_res.status === 200) {
     return res.status(200).json({ success: true });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: "Failed to add you up. Please try again" });
   }
-
-  return res.status(400).json({ error: data });
 };
